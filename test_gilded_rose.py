2b4a90a716f00190dee5a4f886ed9503c430c7e3
Keke Li <Keke@Kekes-MacBook-Pro.local>
2024-10-01 23:53:21 -0700

    Lab4 Submission

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,8 +9,25 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("fixme", items[0].name)
 
+    def test_sulfuras(self):
+        items = [Item("Sulfuras", 15, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].quality)    
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes", 10, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(32, items[0].quality)
+
+    def test_conjured(self):
+        items = [Item("Conjured", 8, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(18, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
